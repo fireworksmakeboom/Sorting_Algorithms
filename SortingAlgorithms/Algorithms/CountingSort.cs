8910fa7374c1814804668727602cff4ab72964f0
Lutz Mitländer <lutzmitlaender@gmx.net>
2022-06-09 18:20:30 +0200

    Add Counting Sort

---
+++ SortingAlgorithms/Algorithms/CountingSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algorithms;
+
+static class CountingSort
+{
+    public static int[] Sort(int[] array, int highestValue)
+    {
+        int[] sortedArray = new int[array.Length];
+        int[] valueArray = new int[highestValue + 1];
+
+        for (int i = 0; i < valueArray.Length; i++)
+        {
+            valueArray[i] = 0;
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            valueArray[array[i]]++;
+        }
+        for (int i = 1; i <= highestValue; i++)
+        {
+            valueArray[i] += valueArray[i - 1];
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            sortedArray[valueArray[array[i]] - 1] = array[i];
+            valueArray[array[i]]--;
+        }
+
+        return sortedArray;
+    }
+}
+
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -2,12 +2,16 @@
 
 int[] unsortedArray = new int[25];
 int[] arrayCopy = new int[25];
+int maxNum = 0;
 
 Random random = new Random();
 
 for (int i = 0; i < unsortedArray.Length; i++)
 {
-    unsortedArray[i] = random.Next(1, 100);
+    unsortedArray[i] = random.Next(1, 1000);
+
+    if (unsortedArray[i] > maxNum) 
+        maxNum = unsortedArray[i];
 }
 
 //unsortedArray = new int[] {62, 11, 28, 62, 64, 57, 93, 13, 44, 17, 98, 55, 24, 40, 20, 5, 76, 70, 55, 98, 37, 83, 27, 4, 24};
@@ -38,6 +42,11 @@ Array.ForEach(arrayCopy, i => Console.Write($"{i}, "));
 Array.Copy(unsortedArray, arrayCopy, 25);
 QuickSort.Sort(arrayCopy, 0, unsortedArray.Length - 1);
 Console.WriteLine("\nSortiert Quicksort: ");
+Array.ForEach(arrayCopy, i => Console.Write($"{i}, "));
+
+Array.Copy(unsortedArray, arrayCopy, 25);
+arrayCopy = CountingSort.Sort(arrayCopy, maxNum);
+Console.WriteLine("\nSortiert Counting Sort: ");
 Array.ForEach(arrayCopy, i => Console.Write($"{i}, "));
 
 Array.Copy(unsortedArray, arrayCopy, 25);
